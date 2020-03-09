a4ba285cdcb41b34485b57abb3e72cbfd92bb571
i-van-develop <smartpagedevelop@gmail.com>
2020-03-09 09:32:47 +0200

    update profile store

--- resources/js/store/profile/index.js
+++ resources/js/store/profile/index.js
@@ -20,14 +20,20 @@ export default {
         }
     },
     actions: {
-        async init({state, commit}){
+        async init({state, commit, dispatch}){
             if (!state.isInit){
                 const userData = await api.getUser();
                 if (userData){
-                    commit(SET_DATA,{isAuth:true, data:userData});
+                    await dispatch('setAuthData',userData);
                 }
                 commit(SET_INIT);
             }
+        },
+        setAuthData({commit}, payload){
+            commit(SET_DATA, {isAuth:true, data:payload});
+        },
+        logout({commit}){
+            commit(SET_DATA, {isAuth: false, data: null});
         }
     }
 }
